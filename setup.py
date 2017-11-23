91e6e0aff795fc17a3deae3be148efcf37d1584b
groverpr <pgrover3@dons.usfca.edu>
2017-11-23 02:25:24 -0800

    contributors updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
   name = 'fastai',
   packages = ['fastai'], # this must be the same as the name above
   version = '0.1',
-  author = 'Jeremy Howard',
+  author = 'Jeremy Howard, Rachel Thomas, Yannet Interian and many others',
   author_email = 'j@fast.ai',
   description = 'The fast.ai deep learning library, lessons, and tutorials',
   url = 'https://github.com/fastai/fastai.git', # use the URL to the github repo
