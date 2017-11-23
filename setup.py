0dea3177e3b8e8b10cde7b6e75ec45e821b379d7
groverpr <pgrover3@dons.usfca.edu>
2017-11-23 00:13:52 -0800

    setup files

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+# coding: utf-8
+
+from distutils.core import setup
+setup(
+  name = 'fastai',
+  packages = ['fastai'], # this must be the same as the name above
+  version = '0.1',
+  description = 'The fast.ai deep learning library, lessons, and tutorials',
+  url = 'https://github.com/fastai/fastai.git', # use the URL to the github repo
+  download_url = 'https://github.com/groverpr/fastai/archive/0.1.tar.gz', 
+  keywords = ['deeplearning', 'pytorch', 'machinelearning'], # arbitrary keywords
+  classifiers = [],
+)
+
+
+
+
+
+
