3715979ff90a88ebf5e64a4cc955ed86e3adea0c
groverpr <pgrover3@dons.usfca.edu>
2017-11-23 00:24:12 -0800

    author email added

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
   name = 'fastai',
   packages = ['fastai'], # this must be the same as the name above
   version = '0.1',
+  author_email = 'j@fast.ai',
   description = 'The fast.ai deep learning library, lessons, and tutorials',
   url = 'https://github.com/fastai/fastai.git', # use the URL to the github repo
   download_url = 'https://github.com/groverpr/fastai/archive/0.1.tar.gz', 
