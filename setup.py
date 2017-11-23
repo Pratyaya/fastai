8f5867322bc3f363d87b3863c13427f1222c6c2d
groverpr <pgrover3@dons.usfca.edu>
2017-11-23 02:37:02 -0800

    0.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   author_email = 'j@fast.ai',
   description = 'The fast.ai deep learning library, lessons, and tutorials',
   url = 'https://github.com/fastai/fastai.git', # use the URL to the github repo
-  download_url = 'https://github.com/groverpr/fastai/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/groverpr/fastai/archive/0.2.tar.gz', 
   keywords = ['deeplearning', 'pytorch', 'machinelearning'], # arbitrary keywords
   classifiers = ['Development Status :: 3 - Alpha'
 			],
