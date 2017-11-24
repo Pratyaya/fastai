566d6080384b4618af6880f6600180ef9b5712ca
groverpr <pgrover3@dons.usfca.edu>
2017-11-23 17:14:01 -0800

    uploading fastai for docker test

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from distutils.core import setup
 setup(
   name = 'fastai',
   packages = ['fastai'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   author = 'Jeremy Howard, Rachel Thomas, Yannet Interian and many others',
   author_email = 'j@fast.ai',
   description = 'The fast.ai deep learning library, lessons, and tutorials',
   url = 'https://github.com/fastai/fastai.git', # use the URL to the github repo
-  download_url = 'https://github.com/groverpr/fastai/archive/0.2.tar.gz', 
+  download_url = 'https://github.com/groverpr/fastai/archive/0.3.tar.gz', 
   keywords = ['deeplearning', 'pytorch', 'machinelearning'], # arbitrary keywords
   classifiers = ['Development Status :: 3 - Alpha'
 			],
